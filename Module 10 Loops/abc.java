32c4f6b95ce67933959f0649540bf1b56814e56a
skmodishashi <skmodi.shashi@gmail.com>
2026-06-09 22:11:24 +0530

    abc

---
+++ Module 10 Loops/abc.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class abc {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String separator = sc.nextLine();
+
+        // code here
+        System.out.print(a+separator+b);
+    }
+}
